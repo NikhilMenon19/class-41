7d58ede5c94547a0809279a751b627e4e1c61383
NikhilMenon19 <77450564+NikhilMenon19@users.noreply.github.com>
2021-04-08 20:28:08 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,81 @@
+class Game {
+    constructor(){
+    }
+    //read the gameState value from the database
+    getState(){
+        database.ref("gameState").on("value",function(data){
+            gameState = data.val()
+        })
+    }
+    //write the gameState value to the database 
+    updateState(state){
+        database.ref("/").update({
+            gameState:state
+        })
+    }
+   start(){
+       if(gameState===0){
+           player=new Player()
+           player.getCount()
+           form=new Form()
+           form.display()
+       }
+       car1=createSprite(100,200);
+       car2=createSprite(300,200);
+       car3=createSprite(500,200) ;
+       car4=createSprite(700,200);
+       car1.addImage(car1img)
+       car2.addImage(car2img)
+       car3.addImage(car3img)
+       car4.addImage(car4img)
+
+       cars=[car1,car2,car3,car4]
+
+   }
+   play(){
+       form.title.hide()
+       form.input.hide()
+       form.button.hide()
+       form.greeting.hide()
+       Player.getPlayersInfo()
+       background("grey")
+       image(trackimg,0,-height*4,width,height*5)
+       if(players!==undefined){
+           var index=0
+           var x=175
+           var y
+           for(var i in players ){
+               index=index+1
+               x=x+200
+               y=height-players[i].distance-40
+               cars[index-1].x=x
+               cars[index-1].y=y
+               if(index===player.index){
+                   fill("green")
+                   ellipse(x,y,60)
+                   camera.position.x=width/2
+                   camera.position.y=cars[index-1].y
+               }
+           }
+       }
+       player.getcarsAtEnd()
+    
+       
+       if(player.distance>3410){
+           gameState=2
+           
+           player.rank+=1
+           Player.updatecarsAtEnd(player.rank)
+       }
+       if(keyDown("up") && player.index!==null){
+           player.distance=player.distance+10
+           player.updateInfo()
+       }
+       drawSprites();
+   }
+
+   end(){
+       console.log("GAME OVER")
+       console.log(player.rank)
+   }
+}
